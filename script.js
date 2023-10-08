e2772e48523841c2e0a5bd0a998e6ca94de4f9a2
norikenpi <norikenpi@gmail.com>
2023-10-08 22:38:52 +0900

    To copy text as I delete it, edit code

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ function removeSpaces() {
     const inputText = inputTextElement.value;
     // 連続するスペースを1つのスペースに置き換える
     const outputText = inputText.replace(/\s+/g, ' ');
-    document.getElementById('outputText').value = outputText;
+    const outputTextElement = document.getElementById('outputText');
+    outputTextElement.value = outputText;
 
     // 入力テキストボックスの中身を空にする
     inputTextElement.value = '';
+
+    // スペースが削除されたテキストをクリップボードにコピー
+    outputTextElement.select();
+    document.execCommand('copy');
 }
+
 function copyToClipboard() {
     const outputText = document.getElementById('outputText');
     const copyButton = document.querySelector('button[onclick="copyToClipboard()"]');
